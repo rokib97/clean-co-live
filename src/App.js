44eb0c23aecb1efef7179fcde1e4590948b1b005
rokib97 <hasanrokib1997@gmail.com>
2022-05-13 15:21:31 +0600

    added basic routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Services from "./pages/Services";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <>
+      <Navbar>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/services" element={<Services />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+      </Navbar>
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Navbar = () => {
+const Navbar = ({ children }) => {
   return (
     <div class="drawer drawer-end">
       <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
@@ -24,27 +24,45 @@ const Navbar = () => {
               </svg>
             </label>
           </div>
-
           <div class="flex-none hidden lg:block">
             <ul class="menu menu-horizontal">
               <li>
-                <a>Navbar Item 1</a>
+                <a className="rounded-lg">Home</a>
               </li>
               <li>
-                <a>Navbar Item 2</a>
+                <a className="rounded-lg">About</a>
+              </li>
+              <li>
+                <a className="rounded-lg">Services</a>
+              </li>
+              <li>
+                <a className="rounded-lg">Contact</a>
+              </li>
+              <li>
+                <a className="rounded-lg">Login</a>
               </li>
             </ul>
           </div>
         </div>
+        {children}
       </div>
       <div class="drawer-side">
         <label for="my-drawer-3" class="drawer-overlay"></label>
         <ul class="menu p-4 overflow-y-auto w-80 bg-base-100">
           <li>
-            <a>Sidebar Item 1</a>
+            <a>Home</a>
           </li>
           <li>
-            <a>Sidebar Item 2</a>
+            <a>About</a>
+          </li>
+          <li>
+            <a>Services</a>
+          </li>
+          <li>
+            <a>Contact</a>
+          </li>
+          <li>
+            <a>Login</a>
           </li>
         </ul>
       </div>
