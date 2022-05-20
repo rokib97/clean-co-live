7928ec77646b146c7f2b66ad14333fd985f4c8d9
rokib97 <hasanrokib1997@gmail.com>
2022-05-20 19:09:40 +0600

    added some little fixes

--- src/pages/Dashboard/AddService.js
+++ src/pages/Dashboard/AddService.js
@@ -1,12 +1,39 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import fetcher from "../../api";
 const AddService = () => {
+  const [imgURL, setImgURL] = useState("");
+  const [loading, setLoading] = useState(false);
   const { register, handleSubmit, reset } = useForm();
   const onSubmit = async (data) => {
-    const res = await fetcher.post("/add-service", data);
+    const serviceData = {
+      ...data,
+      image: imgURL,
+    };
+    const res = await fetcher.post("/add-service", serviceData);
     console.log(res);
     reset();
+    setImgURL("");
+  };
+  const handleUploadImage = (e) => {
+    setLoading(true);
+    const formData = new FormData();
+    const image = e.target.files[0];
+    formData.append("image", image);
+    axios
+      .post(
+        "https://api.imgbb.com/1/upload?key=84ac25e6f0a66515cd8221236b78c8f9",
+        formData
+      )
+      .then((res) => {
+        // console.log(res.data.data.display_url);
+        setImgURL(res.data.data.display_url);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
   return (
     <div className="flex justify-center w-full items-center h-screen bg-accent">
@@ -34,8 +61,30 @@ const AddService = () => {
                 class="input input-bordered"
               />
             </div>
+            <div class="form-control">
+              <label
+                htmlFor="image"
+                class={
+                  loading
+                    ? "btn btn-primary mt-4 loading"
+                    : "btn btn-primary mt-4"
+                }
+              >
+                Upload Image
+              </label>
+              <input
+                type="file"
+                id="image"
+                onChange={handleUploadImage}
+                class="input input-bordered hidden"
+              />
+            </div>
             <div class="form-control mt-6">
-              <button type="submit" class="btn btn-primary">
+              <button
+                type="submit"
+                class="btn btn-primary"
+                disabled={!imgURL ? true : false}
+              >
                 Add Service
               </button>
             </div>
